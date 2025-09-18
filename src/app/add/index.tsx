51c7828ae829696a011d4702bae3727c40f04cc6
zSrLusca <lucas.raniery81@gmail.com>
2025-09-18 16:33:22 -0300

    criei a função para recuperar os inputs de name e url, usando useState

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react"
 import {View, Text,TouchableOpacity} from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 import { router } from "expo-router";
@@ -8,7 +9,12 @@ import { Categories } from "@/components/categories";
 import { Button } from "@/components/button";
 
 export default function Add(){
-    
+const [name, setName] = useState("")
+const [url, setUrl] = useState("")
+
+function handleAdd(){
+    console.log({name, url})
+}
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -20,9 +26,9 @@ export default function Add(){
             <Text style={styles.label}>Selecione uma categoria</Text>
             <Categories/>
             <View style={styles.form}>
-            <Input placeholder="nome" onChangeText={console.log}/>
-            <Input placeholder="Url"/>
-            <Button title="Adicionar"/>
+            <Input placeholder="nome" onChangeText={setName}autoCorrect={false} />
+            <Input placeholder="Url"onChangeText={setUrl}autoCorrect={false}/>
+            <Button title="Adicionar" onPress={handleAdd}/>
             </View>
             
         </View>
